e03f970cb191e9c42b399547f204f49176571c76
gundarohit <88605957+qwerty8686@users.noreply.github.com>
2021-09-07 17:31:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var textInput = document.querySelector("#text");
+
+var output = document.querySelector("#morse");
+
+var button = document.querySelector("#button");
+
+var URL = "https://api.funtranslations.com/translate/morse.json";
+
+function errorHandler(error){
+    console.log(error.message);
+    alert("Server is Down, please try again later");
+}
+
+function changeHandler(){
+    var visitURL = URL +"?text=" + textInput.value;
+
+    fetch(visitURL).then(response => response.json()).then(json => {
+        output.innerText =  json.contents.translated;}).catch(errorHandler);
+}
+button.addEventListener("click",changeHandler);
